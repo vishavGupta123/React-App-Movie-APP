933976d8a374b4748cee578ea2e340fc147d7c21
Vishav Gupta <vishavgupta2809@gmail.com>
2020-04-03 12:35:16 -0700

    Changing the state to add favourites

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,15 @@
 // {
 //     type:'INCREASE_COUNT'
 // }
+
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+
+export function addMovies (movies) {
+    return {
+        type:ADD_MOVIES,
+        movies
+   
+   
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCart';
+import { addMovies } from '../actions';
 
 class App extends React.Component{
   componentDidMount(){
@@ -12,14 +13,11 @@ class App extends React.Component{
     })
     //make api call
     //dispatch action
-    store.dispatch({
-      type:'ADD_MOVIES',
-      movies:data
-    });
-    console.log('STATE',this.props.store.getState());
+    store.dispatch(addMovies(data));
+    console.log('STATE',this.props.store.getState());//list:[], favourites: []
   }
   render(){
-    const movies=this.props.store.getState();
+    const {list}=this.props.store.getState();//list:[], favourites:[]
     console.log('RENDER');
   return (
     <div className="App">
@@ -30,7 +28,7 @@ class App extends React.Component{
           <div className="tab">Favourites</div>
         </div>
         <div className="list">
-          {movies.map((movie,index)=>(
+          {list.map((movie,index)=>(
             <MovieCard movie={movie} key={`movies-${index}`}/>
           ))}
         </div>
