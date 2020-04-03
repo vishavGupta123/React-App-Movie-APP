cf18a647c7ac118fd8534ff5b8006a1d94181129
Vishav Gupta <vishavgupta2809@gmail.com>
2020-04-03 11:25:08 -0700

    Subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCart';
 
-
-function App() {
+class App extends React.Component{
+  componentDidMount(){
+    const {store} = this.props;
+    store.subscribe(()=>{
+      console.log('UPDATED');
+      this.forceUpdate();
+    })
+    //make api call
+    //dispatch action
+    store.dispatch({
+      type:'ADD_MOVIES',
+      movies:data
+    });
+    console.log('STATE',this.props.store.getState());
+  }
+  render(){
+    const movies=this.props.store.getState();
+    console.log('RENDER');
   return (
     <div className="App">
-      Project Setup
+      <Navbar/>
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {movies.map((movie,index)=>(
+            <MovieCard movie={movie} key={`movies-${index}`}/>
+          ))}
+        </div>
+      </div>
     </div>
   );
+  }
+  
+
+
 }
 
 export default App;
